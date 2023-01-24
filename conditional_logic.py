e986c259b002cafd3a91918b96b47836e105bafa
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-24 14:11:13 +0600

    true flase with conditional logic

--- conditional_logic.py
+++ conditional_logic.py
@@ -8,18 +8,39 @@
 
 # print('end')
 
-marks = input('enter your marks:')
-marks = int(marks)
+# marks = input('enter your marks:')
+# marks = int(marks)
 
-if marks >= 80:
-  print('GPA: A')
-elif marks >= 70:
-  print("GPA: B")
-elif marks >= 60:
-  print("GPA: C")
-elif marks >= 50:
-  print("GPA: D")
+# if marks >= 80:
+#   print('GPA: A')
+# elif marks >= 70:
+#   print("GPA: B")
+# elif marks >= 60:
+#   print("GPA: C")
+# elif marks >= 50:
+#   print("GPA: D")
+# else:
+#   print("GPA: F")
+
+# print("done")
+
+n = input("enter number:")
+n = int(n)
+
+if n >= 0 and n % 2 == 0:
+  print(n, "is positive and even number")
+elif n >= 0 and n % 2 != 0:
+  print(n, "is positive and odd number")
+elif n < 0 and n % 2 == 0:
+  print(n, "is negative and even number")
 else:
-  print("GPA: F")
+  print(n, "is negative and odd number")
 
-print("done")
+"""
+if n % 2 == 0:
+  print(n, "is even")
+else:
+  print(n, "is odd")
+"""
+
+print('end')
