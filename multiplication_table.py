d93fe084ccef89f69044577fe22ca2e74895db99
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-24 14:59:22 +0600

    create hardcore multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,13 @@
+n = input("Enter number: ")
+n = int(n)
+
+print(n, "x 1 =", n * 1)
+print(n, "x 1 =", n * 2)
+print(n, "x 1 =", n * 3)
+print(n, "x 1 =", n * 4)
+print(n, "x 1 =", n * 5)
+print(n, "x 1 =", n * 6)
+print(n, "x 1 =", n * 7)
+print(n, "x 1 =", n * 8)
+print(n, "x 1 =", n * 9)
+print(n, "x 1 =", n * 10)
