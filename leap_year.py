85780723cd644b43b2aef886ebffa7171a7d0922
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-24 14:47:45 +0600

    leap year find with short code

--- leap_year.py
+++ leap_year.py
@@ -1,14 +1,8 @@
 year = input("Year: ")
 year = int(year)
 
-if year % 4 == 0:
-  if year % 100 == 0:
-    if year % 400 == 0:
-      print(year, "is leap year")
-    else:
-      print(year, "is not a leap year")
-  else:
-    print(year, "is leap year")
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+  print(year, "is leap year")
 else:
   print(year, "is not a leap year")
 
