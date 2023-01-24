bbac121241976702a19360c3bf10050c677c7b73
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-24 12:50:52 +0600

    variable and data type

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
-print("hello world...!!!")
-print('hello fucking world')
+# print("hello world...!!!")
+# print('hello fucking world')
+
+n1 = 6
+n2 = 2
+n3 = n1 ** n2
+print(n3)
+n1 = 4
+print(n3)
+n3 = n1 ** n2
+print(n3)
