11a90752ecbce3fcca4d573a0549182a1debb3c2
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-24 15:19:18 +0600

    loop and range

---
+++ loop.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "orange", "mango"]
+
+for fruit in fruits:
+  print(fruit)
+
+for i in range(len(fruits)):
+  print(i, fruits[i])
+
+for i in range(3, 10, 2):
+  print(i)
