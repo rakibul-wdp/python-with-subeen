969209d286195f81d598869c0f61d8ad7a43041a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-24 13:46:22 +0600

    conditional logic with result system

--- conditional_logic.py
+++ conditional_logic.py
@@ -1,9 +1,25 @@
-marks = input("enter your marks:")
+# marks = input("enter your marks:")
+# marks = int(marks)
+
+# if marks >= 40:
+#   print("pass")
+# else:
+#   print("fail")
+
+# print('end')
+
+marks = input('enter your marks:')
 marks = int(marks)
 
-if marks >= 40:
-  print("pass")
+if marks >= 80:
+  print('GPA: A')
+elif marks >= 70:
+  print("GPA: B")
+elif marks >= 60:
+  print("GPA: C")
+elif marks >= 50:
+  print("GPA: D")
 else:
-  print("fail")
+  print("GPA: F")
 
-print('end')
+print("done")
