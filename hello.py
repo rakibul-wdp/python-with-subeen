7500b4bb5d08d69e78021b57352b1ded770bd1ba
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-25 10:18:46 +0600

    variables input type conversion

--- hello.py
+++ hello.py
@@ -4,8 +4,32 @@
 n1 = 6
 n2 = 2
 n3 = n1 ** n2
-print(n3)
+# print(n3)
 n1 = 4
-print(n3)
+# print(n3)
 n3 = n1 ** n2
-print(n3)
+# print(n3)
+
+
+age = 22
+price = 19.95
+first_name = "Omok"
+is_online = False
+# print(age)
+
+patient_name = "John Smith"
+age = 20
+is_new = True
+# print(patient_name, age, is_new)
+
+# name = input("What is your name? ")
+# print("hello " + name)
+
+birth_year = input("Enter your birth year: ")
+age = 2020 - int(birth_year)
+print(age)
+
+int()
+float()
+bool()
+str()
--- prog.py
+++ prog.py
@@ -1,5 +1,22 @@
-def multiplication_table(n):
-  for i in range(1, 11):
-    print("{} X {} = {}".format(n, i, n * i))
+# def multiplication_table(n):
+#   for i in range(1, 11):
+#     print("{} X {} = {}".format(n, i, n * i))
 
-multiplication_table(3)
+# multiplication_table(3)
+
+import json
+
+data = '''
+  [
+    { "id" : "001",
+      "x" : "2",
+     "name" : "Quincy"
+    } ,
+    { "id" : "009",
+      "x" : "7",
+      "name" : "Mrugesh"
+    }
+  ]
+'''
+info = json.loads(data)
+print(info[1]['name'])
