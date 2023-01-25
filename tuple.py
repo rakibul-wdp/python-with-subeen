0c3586a24c2e9af334702b196ff0110b03f8a182
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-25 12:12:17 +0600

    tuple

---
+++ tuple.py
@@ -0,0 +1,22 @@
+# li = [1, 2, 3, 5]
+# tpl = (1, 2, 3, 5)
+
+# print(tpl)
+# print(type(tpl))
+# print(tpl[0], tpl[1], tpl[-1])
+
+# for item in tpl:
+#   print(item)
+
+# print(li)
+# print(tpl)
+# li[0] = 10
+# print(li)
+# tpl[0] = 10
+# print(tpl)
+
+tpl = 1, 2, 3
+# print(tpl, type(tpl))
+# print(dir(tuple))
+a, b = tpl
+print(a, b)
