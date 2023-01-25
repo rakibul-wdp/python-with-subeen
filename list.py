30578d431a979b72f2194aa3a6466c9b3336bdeb
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-25 11:58:29 +0600

    more list

---
+++ list.py
@@ -0,0 +1,39 @@
+# list.extend(list2)
+# li = [1, 2, 3, 4]
+# li2 = [5, 6, 7]
+
+# li.extend(li2)
+# print(li)
+
+# li = [1, 2, 3, 5]
+# li.insert(1, 6)
+# print(li)
+
+# li = [1, 2, 3, 5, 3]
+# if 5 in li:
+#   print(li.index(5))
+
+li = [1, 2, 6, 4, 3, 5, 3]
+# li.remove(30)
+# print(li)
+
+# x = li.pop(5)
+# print(x)
+# print(li)
+
+li.sort()
+print(li)
+li.reverse()
+print(li)
+
+# list.insert(index, elem)
+
+# list.index(elem)
+
+# list.remove(elem)
+
+# list.pop(index)
+
+# list.sort()
+
+# list.reverse()
