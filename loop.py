5439a5c70d441da09d236842e3cea091e9870e4f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-01 11:42:20 +0600

    liniar search

--- loop.py
+++ loop.py
@@ -11,8 +11,39 @@
 
 li = [1, 3, 6, 10, 100, 14, 5, 9]
 
-result = min(li)
-print(result)
+key = 66
+if key in li:
+  print("Found")
+else:
+  print("Not Found")
+
+# key = 6
+# found = False
+# for item in li:
+#   if key == item:
+#     found = True
+#     break
+
+# if found is True:
+#   print("Found")
+# else:
+#   print("Not Found")
+
+# key = 66
+# flag = False
+# for item in li:
+#   if key == item:
+#     print("Found")
+#     flag = True
+#     break
+# if flag is False:
+#   print("Not Found")
+
+# found = 5 in li
+# print(found)
+
+# result = min(li)
+# print(result)
 
 # result = 1
 # for num in li:
