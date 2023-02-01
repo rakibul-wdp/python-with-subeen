5cbc6e56bb2908d832a627704925ecb5ead7ceda
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-01 11:26:43 +0600

    learned max min sum

--- loop.py
+++ loop.py
@@ -1,10 +1,35 @@
-fruits = ["apple", "banana", "orange", "mango"]
+# fruits = ["apple", "banana", "orange", "mango"]
 
-for fruit in fruits:
-  print(fruit)
+# for fruit in fruits:
+#   print(fruit)
 
-for i in range(len(fruits)):
-  print(i, fruits[i])
+# for i in range(len(fruits)):
+#   print(i, fruits[i])
 
-for i in range(3, 10, 2):
-  print(i)
+# for i in range(3, 10, 2):
+#   print(i)
+
+li = [1, 3, 6, 10, 100, 14, 5, 9]
+
+result = min(li)
+print(result)
+
+# result = 1
+# for num in li:
+#   result = result * num
+# print(result)
+
+# result = 0
+# for num in li:
+#   result = result + num
+# print(result)
+
+# result = sum(li)
+# print(result)
+
+# max_number = float("-inf")
+# for num in li:
+#   if num > max_number:
+#     max_number = num
+
+# print(max_number)
