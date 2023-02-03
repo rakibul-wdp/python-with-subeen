6332d877096818c85d095aea2f4bbf3cb113189d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-03 09:36:29 +0600

    learned context managers

--- file.py
+++ file.py
@@ -1,11 +1,16 @@
 # fp = open('myfile.txt', 'w')
 # fp = open('myfile.txt', 'r')
-fp = open('myfile.txt', 'a')
+# fp = open('myfile.txt', 'a')
 # fp.write('This is a test file\n')
-lines = ["Apple\n", "Banana\n", "Coconut\n"]
-fp.writelines(lines)
+# lines = ["Apple\n", "Banana\n", "Coconut\n"]
+# fp.writelines(lines)
 
 # content = fp.read()
 # print(content)
 
-fp.close()
+# fp.close()
+
+
+with open("myfile.txt", "r") as fp:
+  content = fp.readlines()
+  print(content)
